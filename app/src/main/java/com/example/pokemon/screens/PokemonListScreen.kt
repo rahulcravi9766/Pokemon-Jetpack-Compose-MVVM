a72c1c7b88618b6dfacaaf6346fd33fa00706e64
DIATOZIAN <rahul.ravi@diatoz.com>
2024-01-02 16:35:27 +0530

    main screen UI and functionality completed

--- app/src/main/java/com/example/pokemon/screens/PokemonListScreen.kt
+++ app/src/main/java/com/example/pokemon/screens/PokemonListScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -33,11 +34,13 @@ import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -47,6 +50,9 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
 import com.example.pokemon.R
 import com.example.pokemon.data.models.PokemonListEntry
 import com.example.pokemon.viewmodel.PokemonListViewModel
@@ -137,24 +143,24 @@ fun PokemonList(
     val loadError by remember { viewModel.loadError }
     val isLoading by remember { viewModel.isLoading }
 
-    LazyColumn(contentPadding = PaddingValues(16.dp)) {
-        val itemCount = if (pokemonList.size % 2 == 0) {
-            pokemonList.size / 2
-        } else {
-            pokemonList.size / 2 + 1
-        }
-    //    val itemCount = pokemonList.size
+    //  LazyColumn(contentPadding = PaddingValues(16.dp)) {
+//        val itemCount = if (pokemonList.size % 2 == 0) {
+//            pokemonList.size / 2
+//        } else {
+//            pokemonList.size / 2 + 1
+//        }
+    //  val itemCount = pokemonList.size
 
 
-        items(itemCount) {
-            if (it >= itemCount - 1 && !endReached) {
-                Log.d(TAG, "end Reached")
-                viewModel.loadPokemonPaginated()
-            }
-            PokemonRow(rowIndex = it, details = pokemonList, navController = navController)
-           // PokemonCardTest(index = it, details = pokemonList, navController = navController)
-        }
-    }
+//        items(itemCount) {
+//            if (it >= itemCount - 1 && !endReached) {
+//                Log.d(TAG, "end Reached")
+//                viewModel.loadPokemonPaginated()
+//            }
+    //PokemonRow(rowIndex = it, details = pokemonList, navController = navController)
+    PokemonCardTest(details = pokemonList, navController = navController)
+    //    }
+    //  }
 }
 
 @Composable
@@ -262,34 +268,100 @@ fun PokemonRow(
 
 
 @Composable
-fun PokemonCardTest(index: Int, details: List<PokemonListEntry>, navController: NavController) {
+fun PokemonCardTest(
+    details: List<PokemonListEntry>,
+    navController: NavController,
+    viewModel: PokemonListViewModel = hiltViewModel()
+) {
+    val pokemonList by remember { viewModel.pokemonList }
+    val endReached by remember { viewModel.endReached }
+    val loadError by remember { viewModel.loadError }
+    val isLoading by remember { viewModel.isLoading }
+    val defaultDominantColor = MaterialTheme.colorScheme.surface
+    val dominantColor by remember {
+        mutableStateOf(defaultDominantColor)
+    }
+    val itemCount = pokemonList.size
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
         contentPadding = PaddingValues(20.dp),
         content = {
             items(details.size) {
+                if (it >= itemCount - 1 && !endReached) {
+                    viewModel.loadPokemonPaginated()
+                }
                 Box(
                     modifier = Modifier
                         .padding(10.dp)
-//                        .background(color = Color.Green)
                         .shadow(5.dp, RoundedCornerShape(10.dp))
                         .clip(RoundedCornerShape(10.dp))
                         .aspectRatio(1f)
-                        .clickable { },
+                        .background(
+                            Brush.verticalGradient(
+                                listOf(
+                                    dominantColor,
+                                    defaultDominantColor
+                                )
+                            )
+                        )
+                        .clickable {
+                            navController.navigate("pokemon_detail_screen/${dominantColor.toArgb()}/${details[it].pokemonName}")
+                        },
                     contentAlignment = Center
                 ) {
-                    AsyncImage(
-                        model = details[index].imageUrl,
-                        contentDescription = details[index].pokemonName,
-                        placeholder = painterResource(
-                            id = R.drawable.pokemon_logo
-                        ),
-                        modifier = Modifier
-                            .size(120.dp)
-                            .align(Center)
-                    )
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        SubcomposeAsyncImage(
+                            model = details[it].imageUrl,
+                            contentDescription = details[it].pokemonName,
+                            modifier = Modifier
+                                .size(120.dp)
+                                .align(CenterHorizontally)
+                        ) {
+                            val state = painter.state
+                            if (state is AsyncImagePainter.State.Loading || state is AsyncImagePainter.State.Error) {
+                                CircularProgressIndicator(
+                                    color = MaterialTheme.colorScheme.primary,
+                                    modifier = Modifier.scale(0.5f)
+                                )
+                            } else {
+                                SubcomposeAsyncImageContent()
+                            }
+                        }
+//                        AsyncImage(
+//                            model = details[it].imageUrl,
+//                            contentDescription = details[it].pokemonName,
+//                            placeholder = painterResource(
+//                                id = R.drawable.pokemon_logo
+//                            ),
+//                            modifier = Modifier
+//                                .size(120.dp)
+//                                .align(CenterHorizontally),
+//                            contentScale = ContentScale.FillBounds,
+//                            onLoading = { loading ->
+//
+////                                if (loading.equals(true)){
+////                                    CircularProgressIndicator(
+////                                        color = MaterialTheme.colorScheme.primary,
+////                                        modifier = Modifier.scale(0.5f)
+////                                    )
+////                                }
+//
+//                            },
+//                            onSuccess = {
+//                                Log.d(TAG, "success $it")
+//                            }
+//                        )
+                        Spacer(modifier = Modifier.width(5.dp))
+                        Text(
+                            text = details[it].pokemonName,
+                            fontFamily = FontFamily.SansSerif,
+                            fontSize = 20.sp,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
                 }
-
             }
         })
 }
+
